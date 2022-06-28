0750e895f6a937bab53bafd8c59eec74106a7135
sch <1572187242@qq.com>
2022-06-28 22:34:34 +0800

    add match.thrift

---
+++ thrift/match.thrift
@@ -0,0 +1,12 @@
+namespace cpp match_service
+struct User
+{
+    1: i32 id,
+    2: i32 score,
+    3: string name
+};
+service Match{
+    i32 add_user(1: User user,2: string info),
+    i32 remove_user(1: User user,2: string info),
+
+}
