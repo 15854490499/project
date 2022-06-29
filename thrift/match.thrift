fa3cea88e1f4e96e093d15eff21ac1f0fa6024df
sch <1572187242@qq.com>
2022-06-29 21:11:01 +0800

    add match_server

--- thrift/match.thrift
+++ thrift/match.thrift
@@ -4,7 +4,7 @@ struct User
     1: i32 id,
     2: i32 score,
     3: string name
-};
+}
 service Match{
     i32 add_user(1: User user,2: string info),
     i32 remove_user(1: User user,2: string info),
